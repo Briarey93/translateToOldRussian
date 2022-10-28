8df2f26e997756a9beb33ef331434cce9735142f
Briarey93 <dima.zjuz@gmail.com>
2022-10-28 15:27:17 +0300

    перегрузка метода. без параметра идёт по default parameter: "text"

--- src/main/java/ru/liga/translateService/TranslateService.java
+++ src/main/java/ru/liga/translateService/TranslateService.java
@@ -6,6 +6,7 @@ import ru.liga.translateService.rules.*;
 
 @Slf4j
 public class TranslateService {
+    private final String DEFAULT_PARAMETER = "text";
 
     public String translateString(final String resource, final String param) {
         log.debug(String.format("Start translate parameter: %s string:\"%s\"", param, resource));
@@ -14,5 +15,9 @@ public class TranslateService {
 
         log.debug(String.format("Finish translate parameter: %s string:\"%s\"", param, result));
         return result;
+    }
+
+    public String translateString(final String resource) {
+        return translateString(resource, DEFAULT_PARAMETER);
     }
 }
